52c6dc081fbc0071ee0af466dac066a992219ab4
aboud20-meet <aboud20@meet.mit.edu>
2018-11-22 18:06:12 +0200

    doneeee

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World test!'
+def home_page():
+    return "MY NAME IS BETER, BETER WITH B , B AS IN BEOBLE."
 
 if __name__ == '__main__':
     app.run(debug=True)
